97e8c507840b305ae19214ab4ddb5101ea59d5eb
serverPc <linkto898@hotmail.com>
2024-03-15 00:51:49 +0500

    ok

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-// const {
-//   ytdown,
-//   tikdown,
-//   ndown,
-//   twitterdown,
-//   fbdown,
-// } = require("nayan-media-downloader");
-
+import express from "express";
+import cors from "cors";
+import { Instagram } from "social-downloader-cherry";
 const app = express();
 app.use(express.json());
 app.use(cors());
