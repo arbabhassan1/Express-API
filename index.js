1cbc93fce47b28a01858e104be40a158a87f7f66
serverPc <linkto898@hotmail.com>
2024-03-15 00:30:45 +0500

    API response

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+const {
+  ytdown,
+  tikdown,
+  ndown,
+  twitterdown,
+  fbdown,
+} = require("nayan-media-downloader");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -12,15 +23,62 @@ app.get("/", (req, res) => {
   res.send(data);
 });
 
-app.get("/fetch", (req, res) => {
+app.get("/fetch", async (req, res) => {
   let { url, site } = req.query;
-  console.log(url, site);
-  res.send({
-    url: url,
-    site: site,
-  });
+
+  try {
+    const videodata = await getdata(url, site);
+    let mystatus = videodata.status;
+
+    const responseObject = {
+      videodata: videodata,
+      media: site,
+      status: mystatus,
+    };
+
+    res.json(responseObject);
+  } catch (error) {
+    console.error("Error:", error);
+
+    // Send an error response
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(3434, () => {
   console.log("Server is Live!");
 });
+
+async function getdata(url, media) {
+  switch (media) {
+    case "youtube":
+      let youtube = await ytdown(url);
+      console.log(youtube);
+      return youtube;
+      break;
+    case "facebook":
+      let facebook = await ndown(url);
+      //  console.log(facebook);
+      return facebook;
+      break;
+    case "instagram":
+      let instagram = await ndown(url);
+      //  console.log(instagram);
+      return instagram;
+      break;
+    case "tiktok":
+      let tiktok = await tikdown(url);
+      //   console.log(tiktok);
+      return tiktok;
+      break;
+    case "twitter":
+      let twitter = await twitterdown(url);
+      //  console.log(twitter);
+      return twitter;
+      break;
+
+    default:
+      return null;
+      break;
+  }
+}
